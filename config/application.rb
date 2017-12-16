19f9d77991e608802414e1012eeb4da215359863
David Sawatske <sawatskeda10@gmail.com>
2017-12-16 14:54:04 -0600

    Config generators for cleaner API

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,12 @@ module BlueChip
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.template_engine false
+      g.test_framework  nil
+      g.stylesheets false
+    end
   end
 end
