e954ab72fdec8db0df9cc4738dda50ae1deab281
David Sawatske <sawatskeda10@gmail.com>
2017-12-16 18:11:42 -0600

    Add auth methods to used by Users and Session Controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,32 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  include ActionController::RequestForgeryProtection
+
+  protect_from_forgery with: :exception, unless: -> { request.format.json? }
+
+  helper_method :current_user, :logged_in?
+
+  private
+    def current_user
+      return nil unless session[:session_token]
+      @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in?
+      !current_user.nil?
+    end
+
+    def login(user)
+      session[:session_token] = user.reset_session_token!
+      @current_user = user
+    end
+
+    def logout
+      current_user.reset_session_token!
+      session[:session_token] = nil
+      @current_user = nil
+    end
+
+    def require_login
+      render json: { base: ['Please log in'] }, status: 401 unless logged_in?
+    end
 end
