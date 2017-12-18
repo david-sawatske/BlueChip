5d34838bf984fffec0db69cb224f4a0f6e0325b5
David Sawatske <sawatskeda10@gmail.com>
2017-12-18 10:37:00 -0600

    Create Leagues model with needed methods

---
+++ app/controllers/api/leagues_controller.rb
@@ -0,0 +1,23 @@
+class Api::LeaguesController < ApplicationController
+  def index
+    @leagues = League.all
+  end
+
+  def show
+    @league = League.find_by(id: params[:id]) || League.random_league
+  end
+
+  def create
+    @league = League.new(league_params)
+    if @league.save
+      render :show
+    else
+      render json: @league.errors.full_messages, status: 422
+    end
+  end
+
+  private
+    def league_params
+      params.require(:league).permit(:name, :starting_balance)
+    end
+end
