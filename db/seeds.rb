3bf3d4985660f6f5cb95531a71574c4b779a2f3a
David Sawatske <sawatskeda10@gmail.com>
2017-12-18 12:09:37 -0600

    Seed development database for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+League.destroy_all
+
+Transaction.destroy_all
+symbols = ['MU', 'CSX', 'INTC', 'AAPL', 'SIRI', 'CSCO', 'FB', 'ROKU', 'MSFT',
+           'NVDA', 'TWTR', 'ORCL', 'P', 'TSLA', 'NFLX', 'AMD']
+
+def time_rand from = 0.0, to = Time.now
+  Time.at(from + rand * (to.to_f - from.to_f))
+end
+
+users = []
+leagues = []
+
+10.times do
+  users << User.create!(username: Faker::Internet.user_name, password: "password")
+end
+
+3.times do
+  leagues << League.create!(name: Faker::GameOfThrones.unique.house, starting_balance: 10000)
+end
+
+
+100.times do
+  Transaction.create!(user_id: users.sample.id,
+                      league_id: leagues.sample.id,
+                      symbol: symbols.sample,
+                      purchase_day: time_rand(365.days.ago),
+                      share_price: rand(1.1...150.9).round(2),
+                      share_quant: rand(300))
+end
+
+
+league_users = {}
+leagues.each { |league| league_users [league.id] = []}
+
+Transaction.all.each { |stock| league_users[stock.league_id] << stock.user_id }
+
+league_users.map { |key, value| value.uniq! }
+
+league_users.each do |key, values|
+  values.each do |v|
+    CashBalance.create!(user_id: v, league_id: key, balance: rand(100..100000))
+  end
+end
