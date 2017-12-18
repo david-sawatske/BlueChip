65ae3c7437bf2a844c5502f486b678bce3c23c43
David Sawatske <sawatskeda10@gmail.com>
2017-12-18 16:06:12 -0600

    Remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   root "static_pages#root"
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create, :show, :destroy]
-    resource :session, only: [:create, :destroy]
+    resources :leagues, only: [:index, :show, :create]
+    resources :users, only: [:create, :show]
+    resources :cash_balances, only: [:create, :update]
+    resources :session, only: [:create, :destroy]
+    resources :transactions, only: [:create]
   end
 end
