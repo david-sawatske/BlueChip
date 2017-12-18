823d716b49c89063cccafa08a913c8e534cf204d
David Sawatske <sawatskeda10@gmail.com>
2017-12-18 14:56:38 -0600

    Create CashBalancesController controller

---
+++ app/controllers/api/cash_balances_controller.rb
@@ -0,0 +1,24 @@
+class Api::CashBalancesController < ApplicationController
+  def create
+    @cash_balance = CashBalance.new(cash_balance_params)
+    if @cash_balance.save
+      render :show
+    else
+      render json: @cash_balance.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @cash_balance = CashBalance.find(params[:id])
+    if @cash_balance.update(cash_balance_params)
+      render :show
+    else
+      render json: @cash_balance.errors.full_messages, status: 422
+    end
+  end
+
+  private
+    def cash_balance_params
+      params.require(:cash_balance).permit(:user_id, :league_id, :balance)
+    end
+end
