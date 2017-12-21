ba30638b2c501bae148acd0686461daa2e508cb2
David Sawatske <sawatskeda10@gmail.com>
2017-12-21 11:15:50 -0600

    Add userLeaguBalance join data to entities piece of state

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,13 +3,15 @@ import { combineReducers } from 'redux';
 import UsersReducer from './users_reducer';
 import LeaguesReducer from './leagues_reducer';
 import TransactionsReducer from './transactions_reducer';
+import userLeagueBalancesReducer from './user_league_balance_reducer';
 import userLeagueTransactionsReducer from './user_league_transaction_reducer';
 
 const EntitiesReducer = combineReducers({
   users: UsersReducer,
   leagues: LeaguesReducer,
   transactions: TransactionsReducer,
-  userLeagueTransactions: userLeagueTransactionsReducer
+  userLeagueBalances: userLeagueBalancesReducer,
+  userLeagueTransactions: userLeagueTransactionsReducer,
 });
 
 export default EntitiesReducer;
---
+++ frontend/reducers/user_league_balance_reducer.js
@@ -0,0 +1,34 @@
+import { combineReducers } from 'redux';
+import { RECEIVE_TARGET_USER_DATA } from '../actions/user_actions';
+
+import { merge, union } from 'lodash';
+
+const userLeagueBalancesById = (state = {}, action) => {
+  switch(action.type) {
+    case RECEIVE_TARGET_USER_DATA:
+      return merge({}, state, action.targetUser
+                                    .userLeagueBalances
+                                    .userLeagueBalancesById);
+
+    default:
+      return state;
+  }
+};
+
+const allUserLeagueBalanceIds = (state = [], action) => {
+  switch(action.type) {
+    case RECEIVE_TARGET_USER_DATA:
+      return union([], state, action.targetUser
+                                    .userLeagueBalances
+                                    .allUserLeagueBalanceIds);
+    default:
+      return state;
+  }
+};
+
+const userLeagueBalancesReducer = combineReducers({
+  userLeagueBalancesById,
+  allUserLeagueBalanceIds
+});
+
+export default userLeagueBalancesReducer;
