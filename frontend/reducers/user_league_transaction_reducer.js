67475ca5c11b8176690997ba6791014019af2a11
David Sawatske <sawatskeda10@gmail.com>
2017-12-22 11:26:48 -0600

    Add League action creators to UserLeagueTransactionsReducer

--- frontend/reducers/user_league_transaction_reducer.js
+++ frontend/reducers/user_league_transaction_reducer.js
@@ -1,34 +1,62 @@
 import { combineReducers } from 'redux';
+
 import { RECEIVE_TARGET_USER_DATA } from '../actions/user_actions';
+import { RECEIVE_ALL_LEAGUES,
+         RECEIVE_TARGET_LEAGUE } from '../actions/league_actions';
 
 import { merge, union } from 'lodash';
 
-const userLeagueTransactionsById = (state = {}, action) => {
+const transactionsById = (state = {}, action) => {
   switch(action.type) {
     case RECEIVE_TARGET_USER_DATA:
       return merge({}, state, action.targetUser
                                     .userLeagueTransactions
-                                    .userLeagueTransactionsById);
-
+                                    .userLeagueTransactionsById
+                  );
+    case RECEIVE_TARGET_LEAGUE:
+      return merge({}, state, action.targetLeague
+                                    .userLeagueTransactions
+                                    .userLeagueTransactionsById
+                  );
+    case RECEIVE_ALL_LEAGUES:
+      return merge({}, state, getLeagueData(action).byId);
     default:
       return state;
   }
 };
 
-const allUserLeagueTransactionIds = (state = [], action) => {
+const allTransactionIds = (state = [], action) => {
   switch(action.type) {
     case RECEIVE_TARGET_USER_DATA:
-      return union([], state, action.targetUser
-                                    .userLeagueTransactions
-                                    .allUserLeagueTransactionIds);
+      return union([], state, action.targetUser.transactions.allTransactionIds);
+    case RECEIVE_TARGET_LEAGUE:
+      return union([], state, action.targetLeague.transactions.allTransactionIds);
+    case RECEIVE_ALL_LEAGUES:
+      return union(state, getLeagueData(action).allIds);
     default:
       return state;
   }
 };
 
+// START selectors //
+const getLeagueData = obj => {
+  let byId = {};
+  let allIds = [];
+
+  Object.values(obj.allLeagues).map(league => {
+    byId = merge({}, byId, league.userLeagueTransactions
+                                 .userLeagueTransactionsById);
+    allIds = union([], allIds, league.userLeagueTransactions
+                                     .allUserLeagueTransactionIds);
+  })
+
+  return { byId: byId, allIds: allIds }
+}
+// END selectors //
+
 const UserLeagueTransactionsReducer = combineReducers({
-  userLeagueTransactionsById,
-  allUserLeagueTransactionIds
+  transactionsById,
+  allTransactionIds
 });
 
 export default UserLeagueTransactionsReducer;
