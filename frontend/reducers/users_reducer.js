269d3cf944cb7e35a18c8021280af9e882a3839a
David Sawatske <sawatskeda10@gmail.com>
2017-12-22 11:26:57 -0600

    Add League action creators to UsersReducer

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 
 import { RECEIVE_TARGET_USER_DATA } from '../actions/user_actions';
+import { RECEIVE_ALL_LEAGUES,
+         RECEIVE_TARGET_LEAGUE } from '../actions/league_actions';
 
 import { merge, union } from 'lodash';
 
@@ -8,6 +10,10 @@ const usersById = (state = {}, action) => {
   switch(action.type) {
     case RECEIVE_TARGET_USER_DATA:
       return merge({}, state, action.targetUser.users.usersById);
+    case RECEIVE_TARGET_LEAGUE:
+      return merge({}, state, action.targetLeague.users.usersById);
+    case RECEIVE_ALL_LEAGUES:
+      return merge({}, state, getLeagueData(action).byId);
     default:
       return state;
   }
@@ -17,10 +23,28 @@ const allUserIds = (state = [], action) => {
   switch(action.type) {
     case RECEIVE_TARGET_USER_DATA:
       return union(state, action.targetUser.users.allUserIds);
+    case RECEIVE_TARGET_LEAGUE:
+      return union(state, action.targetLeague.users.allUserIds);
+    case RECEIVE_ALL_LEAGUES:
+      return union(state, getLeagueData(action).allIds);
     default:
       return state;
   }
 };
+
+// START selectors //
+const getLeagueData = obj => {
+  let byId = {};
+  let allIds = [];
+
+  Object.values(obj.allLeagues).map(league => {
+    byId = merge({}, byId, league.users.usersById);
+    allIds = union([], allIds, league.users.allUserIds)
+  })
+
+  return { byId: byId, allIds: allIds }
+}
+// END selectors //
 
 const UsersReducer = combineReducers({
   usersById,
