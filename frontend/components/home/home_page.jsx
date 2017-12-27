82dfc12c397bd14038425a1a9b416facb256ec68
David Sawatske <sawatskeda10@gmail.com>
2017-12-26 20:55:25 -0600

    Add display of sample LeagueIndex on HomePage

--- frontend/components/home/home_page.jsx
+++ frontend/components/home/home_page.jsx
@@ -1,21 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import LeagueIndexItem from '../league/league_index_item'
+
 class HomePage extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  componentWillMount() {
+  componentDidMount() {
     this.props.requestSampleLeague('random')
   }
 
   render()  {
-    const { currentUser } = this.props;
+    const { currentUser, leagueData, leagueIds } = this.props;
+    const sampleLeagueId = leagueIds[Math.floor(Math.random() * leagueIds.length)];
+
+    let leaderBoard = null;
+    if (sampleLeagueId) {
+      leaderBoard = <LeagueIndexItem league={leagueData[sampleLeagueId]}
+                                     key={sampleLeagueId}/>
+    } else {
+      leaderBoard = <div>loading</div>
+    }
 
     return (
       <div className="">
         <h1>Home</h1>
+        <section id="">
+          <div className="">
+            <h3>Check leaderboars to see where you rank</h3>
+            <div>
+              {leaderBoard}
+            </div>
+          </div>
+        </section>
       </div>
     )
   }
--- frontend/components/home/home_page_container.js
+++ frontend/components/home/home_page_container.js
@@ -1,13 +1,16 @@
 import { connect } from 'react-redux';
 
 import { requestSampleLeague } from '../../actions/league_actions';
+import { getLeagueUserData } from '../../reducers/selectors';
 
 import HomePage from './home_page';
 
 import { requestTargetUserData } from '../../actions/user_actions';
 
 const mapStateToProps = state => ({
-  currentUser: state.session.currentUser
+  leagueIds: state.entities.leagues.allLeagueIds,
+  leagueData: getLeagueUserData(state),
+  currentUser: state.session.currentUser,
 })
 
 const mapDispatchToProps = dispatch => ({
