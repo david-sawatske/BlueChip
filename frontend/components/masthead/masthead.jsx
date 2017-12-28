174b5efd4f2c62e96b13264a846daa2a765283bd
David Sawatske <sawatskeda10@gmail.com>
2017-12-27 19:38:38 -0600

    Separate Masthead from MastheadButtons for reusability

--- frontend/components/masthead/masthead.jsx
+++ frontend/components/masthead/masthead.jsx
@@ -5,47 +5,15 @@ import Modal from 'react-modal';
 import { ModalStyle } from '../../modal/modal_shared';
 
 import SessionFormContainer from '../session_form/session_form_container';
+import MastheadButtons from './masthead_buttons';
 
 class Masthead extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      modalOpen: false,
-      formType: null
-    }
-
-    this.modalToggle = this.modalToggle.bind(this);
-  }
-
-  handleSignout(e) {
-    e.preventDefault();
-    this.props.logout()
-  }
-
-  modalToggle() {
-    this.setState({ modalOpen: !this.state.modalOpen })
-  }
-
-  formTypeToggle(formType) {
-    this.setState({ formType: formType})
-  }
-
-  navLink(formType) {
-    if (formType == 'login') {
-      return (<button className="button" onClick={ (signup) =>
-                 this.formTypeToggle('signup') }>
-                 Sign Up
-               </button>)
-    } else if (formType == 'signup') {
-      return (<button className="button" onClick={ (e) =>
-                this.formTypeToggle('login') }>
-                Sign In
-              </button>) }
   }
 
   render() {
-    const { currentUser } = this.props;
+    const { currentUser, logout } = this.props;
 
     return (
       <div className="masthead">
@@ -53,38 +21,8 @@ class Masthead extends React.Component {
           <h1>BlueChip</h1>
         </Link>
 
-        {currentUser ? (
-          <div className="session-buttons">
-            <button className="button" onClick={ (e) =>
-                this.handleSignout(e) }>
-                Sign Out
-            </button>
-          </div>
-
-        ) : (
-          <div className="session-buttons">
-            <button className="button" onClick={ (e) => {
-              this.formTypeToggle('login'); this.modalToggle(e) }}>
-              Log In
-            </button>
-            <button className="button" onClick={ (e) => {
-              this.formTypeToggle('signup'); this.modalToggle(e) }}>
-              Sign Up
-            </button>
-            <button to="" className="button">Guest Login</button>
-          </div>
-        )}
-
-        <Modal
-          isOpen={this.state.modalOpen}
-          onRequestClose={this.modalToggle}
-          style={ModalStyle}>
-
-          <h1>session</h1>
-          {this.navLink(this.state.formType)}
-          <SessionFormContainer formType={this.state.formType}
-                                currentUser={currentUser}/>
-        </Modal>
+        <MastheadButtons currentUser={currentUser}
+                         logout={logout}/>
       </div>
     );
   }
