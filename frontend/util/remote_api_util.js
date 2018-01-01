1ddeef328a76b9c38aa1fa4e5db5945d88291315
David Sawatske <sawatskeda10@gmail.com>
2018-01-01 09:38:41 -0600

    Add dataTypes to AJAX call to remote stock API

--- frontend/util/remote_api_util.js
+++ frontend/util/remote_api_util.js
@@ -1,6 +1,8 @@
 const ROOT_URL = 'https://api.iextrading.com/1.0/stock/';
 
-export const fetchStockSeries = (symbol, interval = '1d', dataTypes = 'quote,news,chart') => {
+export const fetchStockSeries = (symbol,
+                                 interval = '1d',
+                                 dataTypes = 'logo,quote,news,chart,company,stats') => {
   return $.ajax({
     method: 'GET',
     url: `${ROOT_URL}market/batch?symbols=${symbol}&types=${dataTypes}&range=${interval}&last=5`,
