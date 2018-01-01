9bb60cc384680fdf9ad3c8ce5c721ea1a1077029
David Sawatske <sawatskeda10@gmail.com>
2018-01-01 17:11:55 -0600

    Add customizer for desired state merge results

--- frontend/reducers/remote_stocks_reducer.js
+++ frontend/reducers/remote_stocks_reducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
-import merge from 'lodash/merge';
+
+import { mergeWith} from 'lodash';
 
 import { RECEIVE_STOCK_SERIES,
          RECEIVE_STOCK_SEARCH } from '../actions/remote_stock_actions';
@@ -7,13 +8,21 @@ import { RECEIVE_STOCK_SERIES,
 const remoteStockData = (state = {}, action) => {
   switch(action.type) {
     case RECEIVE_STOCK_SERIES:
-      return merge({}, state, action.stockSeriesData);
+      return mergeWith({}, action.stockSeriesData, state, customizer);
     case RECEIVE_STOCK_SEARCH:
-      return merge({}, state, action.stockSeriesData);
+      return mergeWith({}, action.stockSeriesData, state, customizer);
     default:
       return state;
   }
 };
+
+// START: Selectors //
+function customizer(objValue, srcValue) {
+  if (objValue <= srcValue) {
+    return objValue;
+  }
+}
+// START: Selectors //
 
 const RemoteStocksReducer = combineReducers({
   remoteStockData
