ce6da29ce71e7dd2b6556325a52811ac860dce69
David Sawatske <sawatskeda10@gmail.com>
2018-01-01 19:12:50 -0600

    Add startRemoteFetch ActionCreator to set Loading state

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,5 +1,6 @@
 import * as UserAPIUtil from '../util/user_api_util';
 
+export const START_REMOTE_FETCH = 'START_REMOTE_FETCH';
 export const RECEIVE_TARGET_USER_DATA = 'RECEIVE_TARGET_USER_DATA';
 
 // sync action creators
@@ -8,9 +9,15 @@ export const receiveTargetUserData = user => ({
   targetUser: user
 });
 
+export const startRemoteFetch = () => ({
+  type: START_REMOTE_FETCH
+});
+
 // thunk async action creators
-export const requestTargetUserData = id => dispatch => (
-  UserAPIUtil.fetchTargetUserData(id).then(user => {
-    dispatch(receiveTargetUserData(user))
-  })
-);
+export const requestTargetUserData = id => dispatch => {
+  dispatch(startRemoteFetch());
+  return UserAPIUtil.fetchTargetUserData(id).then(user =>{
+    dispatch(receiveTargetUserData(user));
+    return user;
+  });
+};
