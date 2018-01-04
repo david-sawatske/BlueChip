72ca2a3e41978876bb4ecc19ee044a4d500f8859
David Sawatske <sawatskeda10@gmail.com>
2018-01-03 18:44:02 -0600

    Change Masthead to stateless Component

--- frontend/components/masthead/masthead.jsx
+++ frontend/components/masthead/masthead.jsx
@@ -1,31 +1,17 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
-import Modal from 'react-modal';
 
-import { ModalStyle } from '../../modal/modal_shared';
-
-import SessionFormContainer from '../session_form/session_form_container';
 import MastheadButtons from './masthead_buttons';
 
-class Masthead extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const MastHead = ({ currentUser, logout }) => (
+  <div>
+    <Link to="/" className="title">
+      <h1>BlueChip</h1>
+    </Link>
 
-  render() {
-    const { currentUser, logout } = this.props;
+    <MastheadButtons currentUser={currentUser}
+                     logout={logout}/>
+  </div>
+);
 
-    return (
-      <div className="masthead">
-        <Link to="/" className="title">
-          <h1>BlueChip</h1>
-        </Link>
-
-        <MastheadButtons currentUser={currentUser}
-                         logout={logout}/>
-      </div>
-    );
-  }
-}
-
-export default Masthead
+export default MastHead;
