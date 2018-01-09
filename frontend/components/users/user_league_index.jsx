33c9017e0a15257d97f0ff460c6b8d1ab2b087c5
David Sawatske <sawatskeda10@gmail.com>
2018-01-09 15:53:11 -0600

    Edit to only show data for selected League

--- frontend/components/users/user_league_index.jsx
+++ frontend/components/users/user_league_index.jsx
@@ -3,17 +3,52 @@ import { Route } from 'react-router-dom';
 
 import UserLeaguePortfolio from '../user_league_portfolio/user_league_portfolio'
 
-const UserLeagueIndex = ({ userLeagueData, currentUser }) => (
-  <div className="">
-    <ul>
-      {Object.values(userLeagueData).map(league =>
-        <UserLeaguePortfolio
-          key={league.leagueId}
-          leagueData={league}
-          currentUser={currentUser}/>
-      )}
-    </ul>
-  </div>
-);
+class UserLeagueIndex extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = { targetLeagueId: '' }
+  }
+
+  setShowLeague(e, leagueId) {
+    e.preventDefault();
+
+    this.setState({ targetLeagueId: leagueId })
+  }
+
+  render() {
+    const { userLeagueData, currentUser } = this.props;
+    const targetLeagueId = this.state.targetLeagueId;
+
+    let ShowComponent
+    if (userLeagueData[targetLeagueId]) {
+      const targetLeague = userLeagueData[targetLeagueId];
+
+      ShowComponent = <UserLeaguePortfolio key={targetLeague.leagueId}
+                                           leagueData={targetLeague}
+                                           currentUser={currentUser} />
+    } else {
+      ShowComponent = <h3>Select a League to View</h3>
+    }
+
+    return (
+      <div className="">
+        <ul>
+          {Object.values(userLeagueData).map(league => (
+              <button onClick={(e) => this.setShowLeague(e, league.leagueId)}
+                      key={league.id}>
+                { league.name }
+              </button>
+            )
+          )}
+        </ul>
+
+        { ShowComponent }
+      </div>
+    )
+  }
+};
 
 export default UserLeagueIndex;
