d9c58e9b28ea2c937c9322569de97734e3f48b3d
David Sawatske <sawatskeda10@gmail.com>
2018-01-09 18:23:10 -0600

    Add conditional render of Loader component

--- frontend/components/stock_show/stock_show.jsx
+++ frontend/components/stock_show/stock_show.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-import { Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import Modal from 'react-modal';
-
 import StockSummary from './stock_summary';
 import StockHeader from './stock_header';
 import CompanyData from './company_data';
 import StockChart from './stock_chart';
 import StockNews from './stock_news_index';
+import Loader from '../shared/loader';
 
 class StockShow extends React.Component {
   constructor(props) {
@@ -16,21 +13,29 @@ class StockShow extends React.Component {
   }
 
   render() {
-    const { stockData, interval } = this.props
-    const { quote, chart, logo, company, stats, news } = stockData;
+    const { remoteStockData, interval } = this.props
+
+    let ShowComponent
+    if (remoteStockData) {
+      const { quote, chart, logo, company, stats, news } = remoteStockData;
+      const defaultInterval = '1d'
+
+      ShowComponent =
+        <div>
+          <StockHeader quote={quote}
+                       logo={logo} />
+          <StockSummary quote={quote} />
+          <StockChart chart={chart}
+                      interval={defaultInterval}/>
+          <StockNews news={news} />
+        </div>
+    } else {
+      ShowComponent = <Loader />
+    }
 
     return (
       <div>
-        <StockHeader quote={quote}
-                     logo={logo} />
-        <StockChart chart={chart}
-                    interval={interval} />
-        <StockSummary quote={quote} />
-        <CompanyData company={company}
-                     float={stats.float}
-                     employees={(stats.revenue / stats.revenuePerEmployee)} />
-        <StockNews news={news}
-                   companyName={quote.companyName} />
+        { ShowComponent }
       </div>
     )
   }
