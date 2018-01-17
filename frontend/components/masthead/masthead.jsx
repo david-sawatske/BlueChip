fe684a3d8f59b0e29f77f44753b8d6183b2fab7c
David Sawatske <sawatskeda10@gmail.com>
2018-01-17 13:03:11 -0600

    Add styling to Masthead

--- frontend/components/masthead/masthead.jsx
+++ frontend/components/masthead/masthead.jsx
@@ -4,11 +4,11 @@ import { Route, Link } from 'react-router-dom';
 import MastheadButtons from './masthead_buttons';
 
 const MastHead = props => (
-  <div>
+  <div className="masthead">
     <Link to="/" className="logo">
       <img className="logo" src="http://res.cloudinary.com/sawatskeda10/image/upload/v1516212174/logo_vuxq8f.jpg" />
     </Link>
-    
+
     <Link to="/" className="title">
       <h1>BlueChip</h1>
     </Link>
