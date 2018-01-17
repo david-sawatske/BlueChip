4eeb95c352189a5725e2777c3debd86b45a4e440
David Sawatske <sawatskeda10@gmail.com>
2018-01-17 10:10:49 -0600

    Edit User#create to return new User id JSON as a string

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,7 +4,7 @@ class Api::UsersController < ApplicationController
 
     if @user.save
       login(@user)
-      render json: { id: @user.id, username: @user.username }
+      render json: { id: @user.id.to_s, username: @user.username }
     else
       render json: @user.errors.full_messages, status: 422
     end
