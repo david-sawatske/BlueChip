cd06680984a7c723eb2f9393add477157382270f
David Sawatske <sawatskeda10@gmail.com>
2018-01-26 08:16:28 -0600

    Edit loader to display spinning logo

--- frontend/components/shared/loader.jsx
+++ frontend/components/shared/loader.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Loader = () => (
-  <div id="loader"></div>
+  <div id="loader">
+      <img src="http://res.cloudinary.com/sawatskeda10/image/upload/e_auto_contrast,q_100/v1516937726/cutmypic_1_pxnibw.png" />
+  </div>
 )
 
 export default Loader
