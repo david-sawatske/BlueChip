df4b2bfc6de6fbd194390fcf6bf3f7db1dd52a55
David Sawatske <sawatskeda10@gmail.com>
2018-01-30 16:46:00 -0600

    Add placeholder profile picture to UserAvatar component

--- frontend/components/users/user_avatar.jsx
+++ frontend/components/users/user_avatar.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 const UserAvatar = ({ userData }) => (
   <div className="avatar">
     <h1>{ userData.username }</h1>
+
+    <img src='https://picsum.photos/300?random' alt='user'/>
   </div>
 );
 
