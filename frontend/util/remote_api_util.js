d4416f7553c8b763491a5da289698585959f08fb
David Sawatske <sawatskeda10@gmail.com>
2018-02-07 14:25:57 -0600

    Add defaultDataTypes and the option to add optionalParams to AJAX call

--- frontend/util/remote_api_util.js
+++ frontend/util/remote_api_util.js
@@ -1,10 +1,17 @@
 const ROOT_URL = 'https://api.iextrading.com/1.0/stock/';
 
-export const fetchStockSeries = (symbol,
-                                 interval = '1d',
-                                 dataTypes = 'logo,quote,news,chart,company,stats') => {
+const defaultDataTypes = 'logo,quote,news,chart,company,stats';
+export const fetchStockSeries = ( symbol,
+                                  interval = '1d',
+                                  optionalParams,
+                                  defaultTypes = defaultDataTypes ) => {
+
+  const dataTypes = (optionalParams) ? (optionalParams + defaultTypes)
+                                         :
+                                       defaultTypes;
+
   return $.ajax({
     method: 'GET',
-    url: `${ROOT_URL}market/batch?symbols=${symbol}&types=${dataTypes}&range=${interval}&last=5`,
+    url: `${ROOT_URL}market/batch?symbols=${symbol}&types=${dataTypes}&range=${interval}&last=5`
   })
 }
