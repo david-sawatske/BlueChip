f6c799646ea30f576bc585587274501032e04049
David Sawatske <sawatskeda10@gmail.com>
2018-02-13 10:49:56 -0600

    Add RemoteStock actions using fetchPeers helper fn

--- frontend/actions/remote_stock_actions.js
+++ frontend/actions/remote_stock_actions.js
@@ -1,6 +1,7 @@
 import * as RemoteStockAPIUtil from '../util/remote_api_util';
 
 export const START_REMOTE_STOCK_FETCH = 'START_REMOTE_STOCK_FETCH';
+export const START_REMOTE_PEER_FETCH = 'START_REMOTE_PEER_FETCH';
 export const RECEIVE_STOCK_SEARCH = 'RECEIVE_STOCK_SEARCH';
 
 // sync action creators
@@ -13,6 +14,10 @@ export const startStockRemoteFetch = () => ({
   type: START_REMOTE_STOCK_FETCH
 });
 
+export const startStockPeerFetch = () => ({
+  type: START_REMOTE_PEER_FETCH
+});
+
 // thunk async action creators
 export const requestStockSearch = (symbol, interval, dataTypes) => dispatch => {
   dispatch(startStockRemoteFetch());
@@ -20,5 +25,14 @@ export const requestStockSearch = (symbol, interval, dataTypes) => dispatch => {
   return RemoteStockAPIUtil.fetchStockSeries(symbol, interval, dataTypes)
     .then(stockSeriesData => {
       dispatch(receiveStockSearch(stockSeriesData));
+  });
+}
+
+export const requestStockPeers = tkrStr => dispatch => {
+  dispatch(startStockRemoteFetch());
+
+  return RemoteStockAPIUtil.fetchPeers(tkrStr)
+    .then(stockPeerData => {
+      dispatch(receiveStockSearch(stockPeerData));
   });
 }
