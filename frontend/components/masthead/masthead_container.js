89405f12144eddaea9084434edbec456ef5cae2c
David Sawatske <sawatskeda10@gmail.com>
2018-04-30 14:00:40 -0500

    Add login Action Creator to Masthead via container element

--- frontend/components/masthead/masthead_container.js
+++ frontend/components/masthead/masthead_container.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import Masthead from './masthead';
 
 import { hideModal, showModal } from '../../actions/modal_actions';
-import { logout } from '../../actions/session_actions';
+import { logout, login } from '../../actions/session_actions';
 
 const mapStateToProps = state => ({
   currentUser: state.session.currentUser,
@@ -12,6 +12,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   logout: () => dispatch(logout()),
+  login: user => dispatch(login(user)),
   hideModal: () => dispatch(hideModal()),
   showModal: (modalType, modalProps) => (
     dispatch(showModal(modalType, modalProps))
