39cf2d35c244ae3dca4bde4248d5c6335198f205
David Sawatske <sawatskeda10@gmail.com>
2018-05-02 12:46:34 -0500

    Prevent display of SearchLink when on stock/search path

--- frontend/components/masthead/top_nav.jsx
+++ frontend/components/masthead/top_nav.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { Route, NavLink, Link } from 'react-router-dom'
+import React from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
 
-const TopNav = ({ currentUser }) => {
+const TopNav = ({ currentUser, location }) => {
   let HomeLink
   if (currentUser) {
     const userLink = `/users/${currentUser.id}`;
@@ -12,13 +12,20 @@ const TopNav = ({ currentUser }) => {
     HomeLink = <NavLink className="nav-link" to="/">Home</NavLink>
   }
 
+  let SearchLink
+  if (location.pathname !== "/stocks/search") {
+    SearchLink = <NavLink className="nav-link" to="/stocks/search">
+                    Live Stock Data
+                 </NavLink>
+  }
+
   return (
     <div className="top-nav">
       {HomeLink}
       <NavLink className="nav-link" to="/leagues/new">Create a League</NavLink>
       <NavLink className="nav-link" to="/leagues">Join a League</NavLink>
-      <NavLink className="nav-link" to="/stocks/search">Live Stock Data</NavLink>
+      { SearchLink }
     </div>
   )
 }
-export default TopNav;
+export default withRouter(TopNav);
