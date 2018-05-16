ff486f88d047cdf7ad56ba9516ba68b858507dd8
David Sawatske <sawatskeda10@gmail.com>
2018-05-15 23:56:37 -0500

    Add heading describing StockIndex on UserLeaguePortfolio component

--- frontend/components/user_league_portfolio/user_league_portfolio_header.jsx
+++ frontend/components/user_league_portfolio/user_league_portfolio_header.jsx
@@ -5,8 +5,9 @@ import { numberToCurrency } from '../../util/helper_functions'
 const PortfolioHeader = ({ balance, cashInvested, leagueName, leagueClass }) => (
   <div className={leagueClass}>
     <h1>{leagueName}</h1>
+    <h4>Currently Owned Stocks</h4>
     <h3>Cash Available:</h3> <h2>{numberToCurrency(balance)}</h2>
-    <br/>  
+    <br/>
     <h3>Cash Invested:</h3> <h2>{numberToCurrency(cashInvested)}</h2>
   </div>
 );
